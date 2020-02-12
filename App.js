12324b09aa151ed3769743c648f90dd1d2856818
Daniel Perez <1498@holbertonschool.com>
2020-02-12 13:10:24 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    console.log("tipo de barra: ");
+    console.log(type);
+    console.log("datos: ");
+    console.log(data);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Solicitando permiso para usar la camara</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>no hay acceso o permisos para usar la camara</Text>;
+  }
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'flex-end',
+      }}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+
+      <Text style={styles.text}>apunta a un qr</Text>
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+    backgroundColor: 'white',
+    textAlign: 'center',
+  },
+});
